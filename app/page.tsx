8c25a37e9ec37e081b7459d10bc2c9c09c6f44fd
MINT <pb1123love@gmail.com>
2024-06-11 23:36:41 +0900

    ✨ Update styles odd, even

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ export default function Home() {
     <main className="flex h-screen items-center justify-center bg-gray-100 dark:bg-gray-700">
       <div className="shadow-lgmd:flex-row flex w-full max-w-screen-sm flex-col gap-2 rounded-3xl bg-white p-5 dark:bg-gray-600">
         {["Nico", "Me", "You", "Yourself"].map((person, index) => (
-          <div key={index} className="flex items-center gap-5">
+          <div
+            key={index}
+            className="flex items-center gap-5 rounded-2xl p-2.5 odd:bg-cyan-100 even:bg-gray-100"
+          >
             <div className="size-10 rounded-full bg-blue-500" />
-            <span>{person}</span>
+            <span className="text-lg font-medium">{person}</span>
             <div className="flex size-7 items-center justify-center rounded-full bg-red-500 text-white">
               <span>{index}</span>
             </div>
