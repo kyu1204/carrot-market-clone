191888607061cf04696e2cc42e757851beb82f52
김민규 <minkyu.kim@birdview.kr>
2024-06-13 17:19:41 +0900

    ✨ Improve UI in Home component with animations
    - Updated gap and styling in person cards
    - Added animation to index number
    - Included pulse animation for empty state

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-gray-100 dark:bg-gray-700">
-      <div className="shadow-lgmd:flex-row flex w-full max-w-screen-sm flex-col gap-2 rounded-3xl bg-white p-5 dark:bg-gray-600">
-        {["Nico", "Me", "You", "Yourself"].map((person, index) => (
+      <div className="shadow-lgmd:flex-row flex w-full max-w-screen-sm flex-col gap-3 rounded-3xl bg-white p-5 dark:bg-gray-600">
+        {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
           <div
             key={index}
-            className="flex items-center gap-5 border-b-2 p-2.5 pb-5 last:border-b-0 last:pb-0"
+            className="flex items-center gap-5"
           >
-            <div className="size-10 rounded-full bg-blue-500" />
-            <span className="text-lg font-medium">{person}</span>
-            <div className="flex size-7 items-center justify-center rounded-full bg-red-500 text-white">
-              <span>{index}</span>
+            <div className="size-10 rounded-full bg-blue-400" />
+            <span className="text-lg font-medium empty:size-24 empty:h-5 empty:rounded-full empty:bg-gray-300 empty:animate-pulse">{person}</span>
+            <div className="flex size-6 items-center justify-center rounded-full bg-red-500 text-white relative">
+              <span className="z-10">{index}</span>
+              <div className="size-6 rounded-full bg-red-500 absolute animate-ping" />
             </div>
           </div>
         ))}
