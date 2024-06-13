755bd6819c82db5b1c6b6639e9e95c62e66d262b
김민규 <minkyu.kim@birdview.kr>
2024-06-13 18:18:28 +0900

    ✨ Adjusted styling for submit button 🎨

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center bg-gray-100 dark:bg-gray-700">
-      <div className="shadow-lgmd:flex-row flex w-full max-w-screen-sm flex-col gap-3 rounded-3xl bg-white p-5 dark:bg-gray-600">
-        <div className="group flex flex-col">
-          <input className="w-full bg-gray-100" placeholder="Write your email" />
-          <span className="group-focus-within:block hidden">Make sure your email...</span>
-          <button>Submit</button>
-        </div>
+    <main className="flex h-screen items-center justify-center bg-gray-100 dark:bg-gray-700 p-5">
+      <div className="shadow-lg md:flex-row flex w-full max-w-screen-sm flex-col gap-3 rounded-3xl bg-white p-5 dark:bg-gray-600">
+        <button className="w-full bg-black text-white rounded-sexy-name h-10">Submit</button>
       </div>
     </main>
   );
