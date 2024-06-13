5889971747962101951edf4c35dc3e76a7350949
김민규 <minkyu.kim@birdview.kr>
2024-06-13 17:27:05 +0900

    ✨ Add group hover effect to person's name

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-gray-100 dark:bg-gray-700">
       <div className="shadow-lgmd:flex-row flex w-full max-w-screen-sm flex-col gap-3 rounded-3xl bg-white p-5 dark:bg-gray-600">
-        {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
+        {["Nico", "Me", "You", "Yourself"].map((person, index) => (
           <div
             key={index}
-            className="flex items-center gap-5"
+            className="flex items-center gap-5 group"
           >
             <div className="size-10 rounded-full bg-blue-400" />
-            <span className="text-lg font-medium empty:size-24 empty:h-5 empty:rounded-full empty:bg-gray-300 empty:animate-pulse">{person}</span>
+            <span className="text-lg font-medium empty:size-24 empty:h-5 empty:rounded-full empty:bg-gray-300 empty:animate-pulse group-hover:text-red-500">{person}</span>
             <div className="flex size-6 items-center justify-center rounded-full bg-red-500 text-white relative">
               <span className="z-10">{index}</span>
               <div className="size-6 rounded-full bg-red-500 absolute animate-ping" />
