de734333ab48c7d2a8508504c975c68365ebedd1
MINT <pb1123love@gmail.com>
2024-06-15 13:42:52 +0900

    ⚡️ Update styling for text color to orange, layout, and homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center bg-gray-100 p-5 dark:bg-gray-700">
-      <div className="flex w-full max-w-screen-sm flex-col gap-3 rounded-3xl bg-white p-5 shadow-lg md:flex-row dark:bg-gray-600">
-        <input type="text" />
-        <a>tttt</a>
-        <button className="btn">Submit</button>
+    <div className="flex min-h-screen flex-col items-center justify-between p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
       </div>
-    </main>
+      <div className="flex w-full flex-col items-center gap-3">
+        <Link
+          href="/create-account"
+          className="w-full rounded-md bg-orange-500 py-2.5 text-center text-lg font-medium text-white transition-colors hover:bg-orange-400"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link className="hover:underline" href="/login">
+            로그인
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
