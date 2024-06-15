3763b571151946245cbf2d191a2725c98fab3958
MINT <pb1123love@gmail.com>
2024-06-16 01:20:06 +0900

    ✨ Add social login component to login and create account pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,15 +2,14 @@ import FormButton from "@/components/form-button";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
-export default function CreateAccount() {
+export default function LogIn() {
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
-        <h2>Fill in ther form below to join!</h2>
+        <h2>Log in with email and password.</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <FormInput required type="text" placeholder="Username" errors={[]} />
         <FormInput required type="email" placeholder="Email" errors={[]} />
         <FormInput
           required
@@ -18,13 +17,7 @@ export default function CreateAccount() {
           placeholder="Password"
           errors={[]}
         />
-        <FormInput
-          required
-          type="password"
-          placeholder="Confirm Password"
-          errors={[]}
-        />
-        <FormButton loading={false} text="Create Account" />
+        <FormButton loading={false} text="Log In" />
       </form>
       <SocialLogin />
     </div>
