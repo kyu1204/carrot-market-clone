31d55c1de849374a14ac19172cca65ba4206be9f
MINT <pb1123love@gmail.com>
2024-06-20 19:08:29 +0900

    ✨ Add phone number validation and SMS login functionality.

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,1 +1,45 @@
-export async function smsVerification(prevState: any, formData: FormData) {}
+import { z } from "zod";
+import validator from "validator";
+import { redirect } from "next/navigation";
+
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "Wrong phone format",
+  );
+
+const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+interface IsmsLoginState {
+  token: boolean;
+}
+
+export async function smsLogin(prevState: IsmsLoginState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect("/");
+    }
+  }
+}
