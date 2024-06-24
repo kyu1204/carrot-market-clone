5221dc189381006a185bfcbb728c74a1200fc7ee
MINT <pb1123love@gmail.com>
2024-06-24 22:58:47 +0900

    ✨ Add middleware function for handling incoming requests.

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "./lib/session";
+
+export async function middleware(request: NextRequest) {
+  console.log("hello");
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
