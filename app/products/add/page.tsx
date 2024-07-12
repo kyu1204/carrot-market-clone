15f401a3aa037f4b592b1b28f1ddaf159deee595
김민규 <minkyu.kim@birdview.kr>
2024-07-12 18:47:37 +0900

    ✨ Add uploadProduct form state handling in AddProduct page.

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -6,6 +6,7 @@ import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import { uploadProduct } from "./actions";
 import { z } from "zod";
+import { useFormState } from "react-dom";
 
 const fileSchema = z.object({
   type: z.string().refine((value) => value.includes("image"), {
@@ -17,6 +18,7 @@ const fileSchema = z.object({
 });
 
 export default function AddProduct() {
+  const [state, action] = useFormState(uploadProduct, null);
   const [preview, setPreview] = useState("");
 
   const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -41,7 +43,7 @@ export default function AddProduct() {
 
   return (
     <div>
-      <form action={uploadProduct} className="flex flex-col gap-5 p-5">
+      <form action={action} className="flex flex-col gap-5 p-5">
         <label htmlFor="photo" className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover"
           style={{
             backgroundImage: `url(${preview})`,
@@ -50,12 +52,13 @@ export default function AddProduct() {
           {preview === "" ? (<><PhotoIcon className="size-20" />
             <div className="text-neutral-400 text-sm">
               사진을 추가해주세요.
+              {state?.fieldErrors.photo}
             </div></>) : null}
         </label>
         <input onChange={onImageChange} type='file' id='photo' name='photo' className="hidden" />
-        <Input name="title" required placeholder="제목" type="text" />
-        <Input name="price" required placeholder="가격" type="number" />
-        <Input name="description" required placeholder="자세한 설명" type="text" />
+        <Input name="title" required placeholder="제목" type="text" errors={state?.fieldErrors.title} />
+        <Input name="price" required placeholder="가격" type="number" errors={state?.fieldErrors.price} />
+        <Input name="description" required placeholder="자세한 설명" type="text" errors={state?.fieldErrors.description} />
         <Button text="작성 완료" />
       </form>
     </div>
