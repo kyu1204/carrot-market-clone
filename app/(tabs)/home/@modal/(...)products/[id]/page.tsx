19ec8a4dc69eb5a553ff8e680e4230939513c4e4
MINT <pb1123love@gmail.com>
2025-02-23 22:57:53 +0900

    ✨ feat: add CloseButton component and integrate it
    Introduce a new CloseButton component for improved navigation. 
    Integrate the CloseButton into the loading and modal pages to 
    enhance user experience by allowing users to easily return to 
    the previous page. Refactor modal page to remove redundant 
    close button code and streamline the component structure.

--- app/(tabs)/home/@modal/(...)products/[id]/page.tsx
+++ app/(tabs)/home/@modal/(...)products/[id]/page.tsx
@@ -1,22 +1,70 @@
-"use client";
+import CloseButton from "@/components/close-button";
+import db from "@/lib/db";
+import { PhotoIcon, UserIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
-import { PhotoIcon, XMarkIcon } from "@heroicons/react/24/solid";
-import { useRouter } from "next/navigation";
+async function getProduct(id: number) {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      user: {
+        select: {
+          username: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  return product;
+}
 
-export default function Modal({ params }: { params: { id: string } }) {
-  const route = useRouter();
-  const onCloseClick = () => {
-    route.back();
-  };
+export default async function Modal({ params }: { params: { id: string } }) {
+  const id = Number(params.id);
+  if (isNaN(id)) {
+    return notFound();
+  }
+
+  const product = await getProduct(id);
+  if (!product) {
+    return notFound();
+  }
 
   return (
     <div className="absolute left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-60">
-      <button className="absolute right-5 top-5 text-neutral-200">
-        <XMarkIcon className="size-10" onClick={onCloseClick} />
-      </button>
-      <div className="flex h-1/2 w-full max-w-screen-sm justify-center">
-        <div className="flex aspect-square items-center justify-center rounded-md bg-neutral-700 text-neutral-200">
-          <PhotoIcon className="h-28" />
+      <CloseButton />
+
+      <div className="flex h-2/3 w-full max-w-screen-sm flex-col gap-5 p-5">
+        <div className="relative aspect-square">
+          <Image
+            fill
+            src={product.photo}
+            alt={product.title}
+            className="object-cover"
+          />
+        </div>
+        <div className="flex items-center gap-3 border-b border-neutral-700 p-5">
+          <div className="size-10 overflow-hidden rounded-full">
+            {product.user.avatar ? (
+              <Image
+                width={40}
+                height={40}
+                src={product.user.avatar}
+                alt={product.user.username}
+              />
+            ) : (
+              <UserIcon />
+            )}
+          </div>
+          <div>
+            <h3>{product.user.username}</h3>
+          </div>
+        </div>
+        <div className="p-5">
+          <h1 className="text-2xl font-semibold">{product.title}</h1>
+          <p>{product.description}</p>
         </div>
       </div>
     </div>
